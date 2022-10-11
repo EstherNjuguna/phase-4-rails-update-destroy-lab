60db59ed281132a70258ca231218318fdf09baf1
+ <+>
2022-10-11 14:50:32 +0300

    commit tree de2d6fa25f45edd24693aa615744cb996534ce4d

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,3 +1,5 @@
+
+
 class PlantsController < ApplicationController
 
   # GET /plants
@@ -16,6 +18,28 @@ class PlantsController < ApplicationController
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  #UPDATE /plants
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant, status: :accepted
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+
+  #DELETE /plants
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
   end
 
   private
